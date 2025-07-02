d8a1814ca20acb0edcb1f757ec90db6506b9aba0
Mobassir <mobassirhossen94@gmail.com>
2025-07-02 08:49:40 +0600

    Update train_gpt_xtts.py

--- train_gpt_xtts.py
+++ train_gpt_xtts.py
@@ -347,7 +347,13 @@ def train_gpt(metadatas, num_epochs, batch_size, grad_acumm, output_path, max_au
         gpt_use_perceiver_resampler=True,
     )
     # define audio config
-    audio_config = XttsAudioConfig(sample_rate=22050, dvae_sample_rate=22050, output_sample_rate=24000)
+    #audio_config = XttsAudioConfig(sample_rate=22050, dvae_sample_rate=22050, output_sample_rate=24000)
+    audio_config = XttsAudioConfig(
+    sample_rate=16000,
+    dvae_sample_rate=16000,  
+    output_sample_rate=24000  # keep as is if inference uses 24kHz output
+    )
+
     # training parameters config
 
     config = GPTTrainerConfig()
